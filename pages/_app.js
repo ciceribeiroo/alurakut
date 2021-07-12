b8cdb093e090287346f490c33e05047602e85d18
ciceribeiroo <alicinha2r@gmail.com>
2021-07-12 17:16:31 -0300

    aula-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,36 @@
+  
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #d9e6f6;
+    font-family: sans-serif;
+  }
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  #_next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
